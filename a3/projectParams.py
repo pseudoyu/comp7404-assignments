2b4fb16b53e96147bb9c39d6f6159c04b7dff695
pseudoyu <pseudoyu@connect.hku.hk>
2020-10-04 22:01:32 +0800

    a3 removed

--- a3/projectParams.py
+++
@@ -1,4 +0,0 @@
-STUDENT_CODE_DEFAULT = 'analysis.py,qlearningAgents.py,valueIterationAgents.py'
-PROJECT_TEST_CLASSES = 'reinforcementTestClasses.py'
-PROJECT_NAME = 'Project 3: Reinforcement learning'
-BONUS_PIC = False
