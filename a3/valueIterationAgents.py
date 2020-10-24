df279fa0a936e702dd420aaf0fea20c9d2b9e9fc
pseudoyu <pseudoyu@connect.hku.hk>
2020-10-24 21:51:13 +0800

    a3 - q1 passed

--- a3/valueIterationAgents.py
+++ a3/valueIterationAgents.py
@@ -31,7 +31,35 @@ class ValueIterationAgent(ValueEstimationAgent):
 
         # Write value iteration code here
         "*** YOUR CODE HERE ***"
+        
+        # Initialization
+        newValueDict = {}
+        actionList = []
+        
+        for i in range(self.iterations):
+            
+            # Construct new value dict
+            newValueDict = self.values.copy()
 
+            for state in self.mdp.getStates():
+                
+                # Check state
+                if self.mdp.isTerminal(state): continue
+                
+                # Get action lists
+                actionList = self.mdp.getPossibleActions(state)
+                
+                # Get Q Value
+                QValues = [self.getQValue(state, action) for action in actionList]
+                
+                # Get optimial value
+                optimialValue = max(QValues)
+                newValueDict[state] = optimialValue
+
+            self.values = newValueDict
+            
+            # Counter
+            i += 1
 
     def getValue(self, state):
         """
@@ -46,7 +74,24 @@ class ValueIterationAgent(ValueEstimationAgent):
           value function stored in self.values.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # util.raiseNotDefined()
+
+        # Initialization
+        QValue = .0
+
+        # Get states And prob
+        transStatesAndProbs = self.mdp.getTransitionStatesAndProbs(state, action)
+
+        for newState, prob in transStatesAndProbs:
+            
+            # Get reward and new value
+            reward = self.mdp.getReward(state, action, newState)
+            newValue = self.values[newState]
+            
+            # Calculate Q Value
+            QValue += prob * (reward + self.discount * newValue)
+
+        return QValue
 
     def computeActionFromValues(self, state):
         """
@@ -58,7 +103,24 @@ class ValueIterationAgent(ValueEstimationAgent):
           terminal state, you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # util.raiseNotDefined()
+
+        # Initialization
+        optimialActionDict = util.Counter()
+        optimialAction = ""
+        
+        # Get action list
+        actionList = self.mdp.getPossibleActions(state)
+        
+        for action in actionList:
+            
+            # Construct optimial action dict
+            optimialActionDict[action] = self.getQValue(state, action)
+        
+        # Returns the key with the highest value (best Action)
+        optimialAction = optimialActionDict.argMax()
+        
+        return optimialAction
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
